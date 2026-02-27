b6e69d096dc6b595b239c248b5a2917f74bc72b1
rayko15 <camiloandresf2018@gmail.com>
2026-02-26 21:57:24 -0500

    Añado el main

---
+++ main.py
@@ -0,0 +1,6 @@
+from profesor import Profesor
+
+prof1 = Profesor("Carlos Pérez", 101, "Matemáticas")
+prof1.asignar_materia("Álgebra")
+
+prof1.mostrar_info()
--- profesor.py
+++ profesor.py
@@ -1,10 +1,17 @@
-class Profesor:
-    def __init_(self, nombre, id_profesor, especialidad):
-    self.nombre = nombre
-    self.id_profesor = id_profesor
-    self.especialidad = especialidad
-    self.materias= []
+class profesor:
+    def __init__(self, nombre, id_profesor, especialidad):
+        self.nombre = nombre
+        self.id_profesor = id_profesor
+        self.especialidad = especialidad
+        self.materias = []
 
     def asignar_materias(self, materia):
-        
+        self.materias.append(materia)
 
+    def mostrar_info(self):
+        print(f"Nombre: {self.nombre}")
+        print(f"ID Profesor: {self.id_profesor}")
+        print(f"Especialidad: {self.especialidad}")
+        print("Materias asignadas:")
+        for materia in self.materias:
+            print(f"- {materia}")
