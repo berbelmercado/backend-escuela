e761cfbee370bbb3ed225a6338102fc3ae578912
rayko15 <camiloandresf2018@gmail.com>
2026-02-26 21:43:54 -0500

    Añado la entidad profesor con sus atributos

---
+++ profesor.py
@@ -0,0 +1,10 @@
+class Profesor:
+    def __init_(self, nombre, id_profesor, especialidad):
+    self.nombre = nombre
+    self.id_profesor = id_profesor
+    self.especialidad = especialidad
+    self.materias= []
+
+    def asignar_materias(self, materia):
+        
+
