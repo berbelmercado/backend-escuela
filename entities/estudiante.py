84b952298e51435e0c1ae9580927005e7a76652d
berbelmercado <berbelmercado@gmail.com>
2026-03-19 20:10:07 -0500

    entidad-curso-y-columas-estudiante

--- entities/estudiante.py
+++ entities/estudiante.py
@@ -1,26 +1,23 @@
 import uuid
 from database.config import Base
-from sqlalchemy import Column, DateTime, ForeignKey, String, Text
+from sqlalchemy import Column, DateTime, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
 
 class Estudiante(Base):
+
     __tablename__ = "estudiantes"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id_estudiante = Column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True
+    )
+    cedula = Column(String(20), nullable=False)
     nombre = Column(String(100), nullable=False)
     apellido = Column(String(100), nullable=False)
-    email = Column(String(255), unique=True, nullable=False)
+    email_estudiante = Column(String(255), unique=True, nullable=False)
+    sexo_estudiante = Column(String(8), nullable=False)
     fecha_nacimiento = Column(DateTime, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-
-    # Relación con la tabla de cursos (muchos a muchos)
-    cursos = relationship(
-        "Curso", secondary="estudiante_curso", back_populates="estudiantes"
-    )
-
-    def __repr__(self):
-        return f"<Estudiante(id={self.id}, nombre={self.nombre}, apellido={self.apellido}, email={self.email})>"
+    no_celular = Column(String(20), nullable=True)
+    fecha_creacion = Column(DateTime, server_default=func.now())
