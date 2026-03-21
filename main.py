9b0bd0cfa18e1475f2e7931ad95f3fbef6af5732
berbelmercado <berbelmercado@gmail.com>
2026-03-20 23:10:37 -0500

    feat-agregar-funciones-crud

--- main.py
+++ main.py
@@ -8,25 +8,25 @@ db = SessionLocal()
 
 obj_estudiante = EstudianteCrud(db)
 
-# nuevo = obj_estudiante.crear_estudiante(
-#     cedula="3245345",
-#     nombre="Camilo",
-#     apellido="Pelaez",
-#     email="camiloPelaez@test.com",
-#     sexo="M",
-#     fecha_nacimiento=date(1997, 1, 12),
-#     no_celular="554323898",
-# )
+nuevo = obj_estudiante.crear_estudiante(
+    cedula="3245345",
+    nombre="Camilo",
+    apellido="Pelaez",
+    email="camiloPelaez@test.com",
+    sexo="M",
+    fecha_nacimiento=date(1997, 1, 12),
+    no_celular="554323898",
+)
 
-# print("Creado:", nuevo.id_estudiante)
+print("Creado:", nuevo.id_estudiante)
 
+# consultar estudiantes
+list_estudiante = obj_estudiante.obtener_estudiantes()
 
-# list_estudiante = obj_estudiante.obtener_estudiantes()
-
-# for i in list_estudiante:
-#     print(
-#         f"""cedula: {i.cedula} nombre: {i.nombre} apellido: {i.apellido} email: {i.email} sexo: {i.sexo} fecha de nacimiento: {i.fecha_nacimiento} Numero De Celular: {i.no_celular}"""
-#     )
+for i in list_estudiante:
+    print(
+        f"""cedula: {i.cedula} nombre: {i.nombre} apellido: {i.apellido} email: {i.email} sexo: {i.sexo} fecha de nacimiento: {i.fecha_nacimiento} Numero De Celular: {i.no_celular}"""
+    )
 
 # Consultar 1 solo estudiante
 list_estudiante = obj_estudiante.obtener_estudiante(
