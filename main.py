74bf5fa5117fe751b57ef85f120a29f7056797d1
berbelmercado <berbelmercado@gmail.com>
2026-03-20 22:34:49 -0500

    feat-correciones-crud-y-main

---
+++ main.py
@@ -0,0 +1,19 @@
+from database.config import SessionLocal
+from crud.estudiante_crud import EstudianteCrud
+from datetime import date
+
+db = SessionLocal()
+
+obj_estudiante = EstudianteCrud(db)
+
+nuevo = obj_estudiante.crear_estudiante(
+    cedula="123456",
+    nombre="Juan",
+    apellido="Perez",
+    email="juan@test.com",
+    sexo="M",
+    fecha_nacimiento=date(2000, 1, 1),
+    no_celular="3041108410",
+)
+
+print("Creado:", nuevo.id_estudiante)
